df8e337f188e010eba2f8defdcb835aa9a1b7b04
DSaltykov <spitfiredia@gmail.com>
2023-10-23 18:18:20 +0300

    push all

---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,21 @@
+package helpers;
+
+import drivers.BrowserStackDriver;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+
+public class Browserstack extends BrowserStackDriver {
+    public static String videoUrl(String sessionId) {
+        String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        return given()
+                .auth().basic(authConfig.getUsername(), authConfig.getPassword())
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().path("automation_session.video_url");
+    }
+}
---
+++ src/test/java/tests/AndroidTests.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+
+@Tag("android")
+public class AndroidTests extends TestBase {
+    @Test
+    @DisplayName("Проверка непустой выдачи при запросе")
+    void successfulSearchTest() {
+        step("Ввести запрос", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+
+        step("Проверить что выдача не пустая", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("Проверка отображения иконки ошибки при переходе в статью")
+    void checkErrorIcon() {
+        step("Ввести запрос", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+
+        step("Проверить что выдача не пустая", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+
+        step("Кликнуть на первый элемент", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_container")).first().click());
+
+        step("Проверить что на странице отображается иконка ошибки", () -> {
+            $(id("org.wikipedia.alpha:id/view_wiki_error_icon")).should(exist);
+        });
+    }
+}
